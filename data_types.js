9d4082f29f7a816a7c1e777452c5b4fa55a11499
Ercan <70813122+234ercan@users.noreply.github.com>
2022-03-25 05:57:23 +0000

    Add files via upload

---
+++ data_types.js
@@ -0,0 +1,19 @@
+var myName = 'Ercan';
+var myLName = 'Yilmaz';
+console.log(myName + " " + myLName);
+var myNum1 = "5";
+var myNum2 = 5.8;
+var myDec1 = true;
+var myDec2 = false;
+var s1 = '';
+var n = null;
+var notDef;
+var isRich=undefined; //NEVER DO THIS
+console.log(typeof(myNum1));
+console.log(typeof(myNum2));
+console.log(typeof(myDec1));
+console.log(typeof(myDec2));
+console.log(typeof(s1));
+console.log(typeof(n));
+console.log(typeof(notDef));
+console.log(typeof(isRich));
