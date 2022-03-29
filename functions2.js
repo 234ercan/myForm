6b94bedd046e026d31f916c8c95c25901c0031d2
Ercan <70813122+234ercan@users.noreply.github.com>
2022-03-29 06:27:28 +0100

    Add files via upload

--- functions2.js
+++ functions2.js
@@ -3,12 +3,10 @@ Functions :
 A function performs a task or calculates a value
 A function can have a return keyword
 */
-console.clear();
 //creating a function with return keyword
 function sum(){
   var x=5;
   var y=7;
-  
   return x+y;
 }
 //calling sum function
@@ -25,3 +23,13 @@ function addNums(num1=4,num2=5,num3=6){
   return num1+num2+num3;
 }
 console.log(addNums());//4+5+6 = 15
+//we can assign new values
+console.log(addNums(1,2,3));//1+2+3=6
+//we can assign new values
+console.log(addNums(1,2));//1+2+6=9
+////Craete a function that returns a string
+function personInfo(name, age, city){
+  
+  return 'Hi '+name+'. '+'You are '+age+' years old. You are from '+city;
+}
+console.log(personInfo('John',50,'LA'));
